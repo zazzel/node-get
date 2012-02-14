4ba0f7b76b7e5928a88fe2c91b39b0ccf7935dee
Konstantin Käfer <github@kkaefer.com>
2012-02-14 23:58:32 +0100

    switch tests to mocha

--- Makefile
+++ Makefile
@@ -2,14 +2,8 @@
 # Run all tests
 #
 
-ifndef only
 test:
-	./node_modules/.bin/expresso -I lib test/*.test.js
-else
-test:
-	rm -rf test_data/files_*
-	./node_modules/.bin/expresso -I lib test/${only}.test.js
-endif
+	@mocha -R spec
 
 lint:
 	./node_modules/.bin/jshint lib/node-get/*.js
--- test/basic.test.js
+++ test/basic.test.js
@@ -1,157 +1,162 @@
-var path = require('path'),
-    util = require('util'),
-    assert = require('assert'),
-    crypto = require('crypto'),
-    _ = require('underscore'),
-    exec = require('child_process').exec,
-    fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+var crypto = require('crypto');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var get = require('..');
 
-function md5(str) {
-    return crypto.createHash('md5').update(str).digest('hex');
+
+function md5(obj) {
+    if (!Array.isArray(obj)) obj = [ obj ];
+    var hash = crypto.createHash('md5');
+    obj.forEach(hash.update.bind(hash));
+    return hash.digest('hex');
 }
 
-var get = require('../lib/node-get');
+var files = [
+    {
+        url: 'https://docs.google.com/a/dbsgeo.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AqV4OJpywingdFNYLXpKMmxqMG1lWTJzNE45ZUVnNlE&single=true&gid=0&output=csv',
+        bin: false,
+        redirects: 1,
+        md5: 'a4d019d2bfedc55e84447f833ed71dff'
+    },
+    {
+        url: 'http://tilemill-data.s3.amazonaws.com/images/paperfolds_256.png',
+        bin: true,
+        md5: 'a99502016d4e2124cf2dc4775aafc256'
+    },
+    {
+        url: 'http://tilemill-data.s3.amazonaws.com/test_data/ipsum.json',
+        bin: true,
+        md5: '651ea0ff31786e9be9012112b21573be'
+    },
+    {
+        url: 'http://tilemill-data.s3.amazonaws.com/test_data/README.txt',
+        bin: false,
+        md5: '46d883d71e795d7c8a65c9b1a6673dd2'
+    },
+    {
+        url: 'http://tilemill-data.s3.amazonaws.com/test_data/shape_demo.zip',
+        bin: true,
+        md5: 'b5ff3545922cc9e0c750fb7263a7a3d3'
+    },
+    {
+        url: 'http://tilemill-data.s3.amazonaws.com/nasa_raster/lasvegas_tm5_12jan09_crop_geo_merc_small.tif',
+        bin: true,
+        md5: '3b97134839042fcab0d0ee8ea76f73c4'
+    },
+    {
+        url: 'http://dcatlas.dcgis.dc.gov/catalog/download.asp?downloadID=2315&downloadTYPE=ESRI',
+        bin: true,
+        md5: 'ac629823b373e7308087264d9f00ef5c'
+    }
+];
+
 
 
 // Ensure that we start over with a clean test_data directory.
-exec('rm -rf ./test_data && mkdir ./test_data', function(err, stdout, stderr) {
-if (err) throw err;
+before(function(done) {
+    exec('rm -rf ./test_data && mkdir ./test_data', done);
+});
 
-exports['test invalid url'] = function() {
-    var req = {
-        uri: 'http://\\/',
-        headers: {
-            'User-Agent': 'tombot'
-        }
-    };
-    new get(req).toDisk('test_data/file_tmp', function(err, result) {
-        assert.equal(err.message, 'Invalid URL');
-    });
-};
-
-exports['test constructor'] = function(beforeExit) {
-    // Verify callback completion.
-    var completed = {
-        perform: 0,
-        toDisk: 0,
-        asString: 0,
-        asBuffer: 0
-    };
-
-    var files = [
-        {
-            url: 'https://docs.google.com/a/dbsgeo.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AqV4OJpywingdFNYLXpKMmxqMG1lWTJzNE45ZUVnNlE&single=true&gid=0&output=csv',
-            bin: false,
-            redirects: 1,
-            md5: 'a4d019d2bfedc55e84447f833ed71dff'
-        },
-        {
-            url: 'http://tilemill-data.s3.amazonaws.com/images/paperfolds_256.png',
-            bin: true,
-            md5: 'a99502016d4e2124cf2dc4775aafc256'
-        },
-        {
-            url: 'http://tilemill-data.s3.amazonaws.com/test_data/ipsum.json',
-            bin: true,
-            md5: '651ea0ff31786e9be9012112b21573be'
-        },
-        {
-            url: 'http://tilemill-data.s3.amazonaws.com/test_data/README.txt',
-            bin: false,
-            md5: '46d883d71e795d7c8a65c9b1a6673dd2'
-        },
-        {
-            url: 'http://tilemill-data.s3.amazonaws.com/test_data/shape_demo.zip',
-            bin: true,
-            md5: 'b5ff3545922cc9e0c750fb7263a7a3d3'
-        },
-        {
-            url: 'http://tilemill-data.s3.amazonaws.com/nasa_raster/lasvegas_tm5_12jan09_crop_geo_merc_small.tif',
-            bin: true,
-            md5: '3b97134839042fcab0d0ee8ea76f73c4'
-        },
-        {
-            url: 'http://dcatlas.dcgis.dc.gov/catalog/download.asp?downloadID=2315&downloadTYPE=ESRI',
-            bin: true,
-            md5: 'ac629823b373e7308087264d9f00ef5c'
-        }
-    ];
-
-    files.forEach(function(reference, i) {
-        var req = {
-            uri: reference.url,
-            headers: {
-                'User-Agent': 'tombot'
-            }
-        };
-
-        new get(req).perform(function(err, result) {
-            var body = [];
-            if (reference.error) {
-                assert.eql(err, reference.error, 'This should have had an error');
-            } else if (err) {
-                throw new Error(err);
-            } else {
-                result.on('data', function(chunk) {
-                    body.push(chunk);
-                });
-                result.on('end', function() {
-                    completed.perform++;
-                    assert.ok(body.length);
-                });
-                result.on('close', function() {
-                    completed.perform++;
-                    assert.ok(body.length);
-                });
-            }
-        });
-
-        new get(req).toDisk('test_data/file_' + i, function(err, result) {
-            completed.toDisk++;
-            if (reference.error) {
-                assert.equal(err.message, reference.error);
-            } else if (err) {
-                throw new Error(err);
-            } else {
-                assert.equal(md5(fs.readFileSync(result)), reference.md5);
-            }
-        });
-
-        new get(req).asString(function(err, result) {
-            completed.asString++;
-
-            if (reference.bin && err) {
-                assert.equal(err.message, "Can't download binary file as string");
-            } else if (reference.error) {
-                assert.equal(err.message, reference.error);
-            } else if (err) {
-                throw new Error(err);
-            } else {
-                assert.equal(md5(result), reference.md5);
-            }
-        });
-
-        new get(req).asBuffer(function(err, result) {
-            completed.asBuffer++;
-
-            if (reference.error) {
-                assert.equal(err.message, reference.error);
-            } else if (err) {
-                throw err;
-            } else {
-                assert.equal(md5(result), reference.md5);
-            }
+describe('get().perform', function() {
+    files.forEach(function(reference) {
+        it('should return request object for ' + reference.url, function(done) {
+            this.timeout(0);
+            new get({
+                uri: reference.url,
+                headers: { 'User-Agent': 'tombot' }
+            }).perform(function(err, result) {
+                var body = [];
+                if (reference.error) {
+                    assert.ok(err);
+                    assert.eql(err.message, reference.error);
+                    done();
+                } else if (err) {
+                    done(err);
+                } else {
+                    result.on('error', done);
+                    result.on('data', function(chunk) {
+                        body.push(chunk);
+                    });
+                    result.on('end', function() {
+                        assert.equal(md5(body), reference.md5);
+                        done();
+                    });
+                }
+            });
         });
     });
+});
 
-    beforeExit(function() {
-        assert.deepEqual(completed, {
-            perform: files.length /* + _.reduce(_.pluck(files, 'redirects'),
-                function(memo, num) { return memo + (num || 0); }, 0) */,
-            toDisk: files.length,
-            asString: files.length,
-            asBuffer: files.length
+describe('get().toDisk', function() {
+    files.forEach(function(reference) {
+        it('should save to disk for ' + reference.url, function(done) {
+            this.timeout(0);
+            new get({
+                uri: reference.url,
+                headers: { 'User-Agent': 'tombot' }
+            }).toDisk('test_data/file_' + reference.md5, function(err, result) {
+                if (reference.error) {
+                    assert.ok(err);
+                    assert.equal(err.message, reference.error);
+                    done();
+                } else if (err) {
+                    done(err);
+                } else {
+                    assert.equal(md5(fs.readFileSync(result)), reference.md5);
+                    done();
+                }
+            });
         });
     });
-};
+});
+
+describe('get().asString', function() {
+    files.forEach(function(reference) {
+        it('should return as string for ' + reference.url, function(done) {
+            this.timeout(0);
+            new get({
+                uri: reference.url,
+                headers: { 'User-Agent': 'tombot' }
+            }).asString(function(err, result) {
+                if (reference.bin && err) {
+                    assert.equal(err.message, "Can't download binary file as string");
+                    done();
+                } else if (reference.bin) {
+                    done(new Error('Should not return binary files as string'));
+                } else if (reference.error) {
+                    assert.equal(err.message, reference.error);
+                    done();
+                } else if (err) {
+                    done(err);
+                } else {
+                    assert.equal(md5(result), reference.md5);
+                    done();
+                }
+            });
+        });
+    });
+});
+
+describe('get().asBuffer', function() {
+    files.forEach(function(reference) {
+        it('should return as buffer for ' + reference.url, function(done) {
+            this.timeout(0);
+            new get({
+                uri: reference.url,
+                headers: { 'User-Agent': 'tombot' }
+            }).asBuffer(function(err, result) {
+                if (reference.error) {
+                    assert.equal(err.message, reference.error);
+                    done();
+                } else if (err) {
+                    done(err);
+                } else {
+                    assert.equal(md5(result), reference.md5);
+                    done();
+                }
+            });
+        });
+    });
 });
